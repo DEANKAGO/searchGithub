6fa0d6b79559de1c9a2876396ed2434b5df9fc60
deankago <martokhago@gmail.com>
2023-10-06 19:12:31 -0400

    started designing footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  imageUrl: string = "assets/images/logo.png";
 
 }
