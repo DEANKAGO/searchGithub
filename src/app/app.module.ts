a6a83bd8a48091e7ecf38501ee4bf7edb7bb37c2
deankago <martokhago@gmail.com>
2023-10-19 14:44:11 -0400

    added search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component'; 
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
