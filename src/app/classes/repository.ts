4e360043dd62d780d222a500824412c911ad16bc
deankago <martokhago@gmail.com>
2023-10-28 06:54:10 -0400

    added repositories class

--- src/app/classes/repository.ts
+++ src/app/classes/repository.ts
@@ -1,2 +1,13 @@
 export class Repository {
+    constructor(
+        public name: string,
+        public description:string,
+        public author:string,
+        public homepage:string,
+        public stargazer:number,
+        public forks:number,
+        public watchers:number,
+        public created_at:Date,
+        public htmlURL:string
+    ){}
 }
