79dba50b43a7c22409e7a2321aa475fac7837d39
deankago <martokhago@gmail.com>
2023-10-29 16:48:28 -0400

    finished adding search functions for users and repos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,7 +10,28 @@ import { UsersService } from '../../services/users.service';
 })
 export class HomeComponent {
 
-  constructor() {}
+  inputData: string='';
+
+  constructor(private info:UsersService, public router: Router) {}
+
+  userSearch() {
+    if (this.inputData==='') {
+      alert("Search field cannot be empty");
+      return;
+    }
+    this.info.userApi(`@api.github.com/users/${this.inputData}`)
+    this.router.navigate([''])
+  }
+
+  repoSearch() {
+    if (this.inputData==='') {
+      alert("Search field cannot be empty");
+      return;
+    }
+    this.info.repoSearch(`@api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=17`)
+    this.router.navigate([''])
+  }
+
 
 
 
